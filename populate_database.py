95f73e34175b56068b000ac099d1af6af56c9b47
MAARTEN\maarten <maartenmeyns@yahoo.com>
2022-07-28 19:58:20 +0200

    Opkuisen

--- populate_database.py
+++ populate_database.py
@@ -1,5 +1,3 @@
-# Load list of websites
-# urls = []
 import pprint
 import random
 import sqlite3
@@ -9,7 +7,8 @@ from sys import platform
 # This file is used to populate the database for the big crawl, it includes the sites from the manual crawl
 
 BASE_PATH = "d:/temp/Selenium-model/"
-aerts_country_list = ['fr', 'ie', 'nl', 'co.uk', 'ch', 'lu', 'be', 'es', 'it', 'tr', 'se', 'pt', 'mt', 'fi', 'si', 'ro', 'de', 'lt', 'lv', 'gr', 'cz', 'hu', 'hr', 'sk', 'ee', 'no', 'bg', 'pl']
+aerts_country_list = ['fr', 'ie', 'nl', 'co.uk', 'ch', 'lu', 'be', 'es', 'it', 'tr', 'se', 'pt',
+                      'mt', 'fi', 'si', 'ro', 'de', 'lt', 'lv', 'gr', 'cz', 'hu', 'hr', 'sk', 'ee', 'no', 'bg', 'pl']
 
 if platform == "linux" or platform == "linux2":
     BASE_PATH = "./data/" # Ubuntu
@@ -189,23 +188,27 @@ print('setting up database')
 conn = sqlite3.connect(BASE_PATH + 'cookies.db')
 cursor = conn.cursor()
 cursor.execute(
-    "CREATE TABLE if not exists visits (visit_id bigint, site_nr int, sitename varchar(255), visit_type int, site_url varchar(255), cookie_numbers int)")
+    "CREATE TABLE if not exists visits (visit_id bigint, site_nr int, sitename varchar(255), "
+    "visit_type int, site_url varchar(255), cookie_numbers int)")
 cursor.execute(
-    "CREATE TABLE if not exists cookies (visit_id bigint, before_after varchar(24), short_url varchar(255), domain varchar(255), expires float(24), httpOnly bool, name varchar(255), path varchar(255), priority varchar(24), sameParty bool, sameSite varchar(25), secure bool, session bool, size int, sourcePort int, sourceScheme varchar(255), value varchar(255))")
+    "CREATE TABLE if not exists cookies (visit_id bigint, before_after varchar(24), short_url varchar(255), "
+    "domain varchar(255), expires float(24), httpOnly bool, name varchar(255), path varchar(255), priority varchar(24), "
+    "sameParty bool, sameSite varchar(25), secure bool, session bool, size int, sourcePort int, sourceScheme varchar(255), value varchar(255))")
 cursor.execute(
-    "CREATE TABLE if not exists elements (visit_id bigint, site_nr int, sitename varchar(255), element_type tinyint, visited tinyint, result varchar(255), element_text varchar(1024), element_css varchar(255), iframe_css varchar(255), location_x int, location_y int, text_color varchar(255), background_color varchar(255), width varchar(24), height varchar(24), font_size varchar(24), PRIMARY KEY (site_nr, element_type))")
+    "CREATE TABLE if not exists elements (visit_id bigint, site_nr int, sitename varchar(255), element_type tinyint, "
+    "visited tinyint, result varchar(255), element_text varchar(1024), element_css varchar(255), iframe_css varchar(255), "
+    "location_x int, location_y int, text_color varchar(255), background_color varchar(255), width varchar(24), height varchar(24), font_size varchar(24), PRIMARY KEY (site_nr, element_type))")
 cursor.execute(
-    "CREATE TABLE if not exists redirects (visit_id bigint, status_code int, date varchar(32), url_from varchar(1024), url_to varchar(1024), content_type varchar(264))")
+    "CREATE TABLE if not exists redirects (visit_id bigint, status_code int, date varchar(32), url_from varchar(1024), "
+    "url_to varchar(1024), content_type varchar(264))")
 cursor.execute(
-    "CREATE TABLE if not exists all_requests (visit_id bigint, status_code int, date varchar(32), url_from varchar(1024), url_to varchar(1024), content_type varchar(264))")
+    "CREATE TABLE if not exists all_requests (visit_id bigint, status_code int, date varchar(32), "
+    "url_from varchar(1024), url_to varchar(1024), content_type varchar(264))")
 cursor.execute(
-    "CREATE TABLE if not exists predictions (visit_id bigint, element_type varchar(32), element_type2 varchar(32), element_text varchar(1024), prediction varchar(32))")
+    "CREATE TABLE if not exists predictions (visit_id bigint, element_type varchar(32), element_type2 varchar(32), "
+    "element_text varchar(1024), prediction varchar(32))")
 # Fill in all rows of to be visited websites if they do not exist
 for url in final_urls:
-    #print(url)
-    # cursor.execute("INSERT OR IGNORE INTO cookie_numbers VALUES (?,?,?,?,?,?,?,?,?)", (url[0], url[1], None, None, None, None, None, None, None))
-    #cursor.execute("INSERT OR IGNORE INTO cookie_numbers (site_nr, sitename) VALUES (?,?)",
-    #               (url[0], url[1]))
     cursor.execute("INSERT OR IGNORE INTO elements (site_nr, sitename, element_type, visited) VALUES (?,?,?,?)",
                    (url[0], url[1], 0, 0))
 cursor.execute('commit')
